f9aa785e0db7295fbcb3c277f617e04a0c011956
Mike Spears <mikespears@inside-65-61-213.wireless.utoronto.ca>
2016-11-23 11:11:28 -0500

    Initial Commit

---
+++ AudioKitTabDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AudioKitTabDemo
+//
+//  Created by Mike Spears on 2016-11-23.
+//  Copyright © 2016 MADLab. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
